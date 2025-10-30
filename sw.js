dc5bf157325843c58d1609c093d142e54e2a4998
myew1980-del <myew1980@gmail.com>
2025-10-30 14:12:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
-self.addEventListener('install', e=> self.skipWaiting());
-self.addEventListener('activate', e=> self.clients.claim());
-self.addEventListener('fetch', ()=>{});
+// sw.js
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
+
+// 這段很關鍵：把所有請求直接轉發到網路（包含跨網域的 POST 到 GAS）
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
