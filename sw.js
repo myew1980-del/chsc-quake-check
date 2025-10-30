2491bce5932c1409910e7f876355b369510ff168
myew1980-del <myew1980@gmail.com>
2025-10-30 14:22:21 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-// sw.js  v2
+// sw.js v3 - 強制轉發所有請求到網路
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => self.clients.claim());
 
-// 關鍵：任何請求（含跨網域的 fetch 到 GAS）都直接轉發到網路，並把回應回傳給頁面
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request).catch(() => new Response("offline")));
 });
